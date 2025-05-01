819d7f089be543d959e3643e4702c6c5c52bb610
wwysopal <wioletta.wysopal@gmail.com>
2025-05-01 10:04:57 +0200

    Integrate ESLint with Prettier

--- ESLintInitialization/eslint.config.mjs
+++ ESLintInitialization/eslint.config.mjs
@@ -1,20 +1,18 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import { defineConfig } from 'eslint/config';
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs}'],
+    files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
-    extends: ['js/recommended'],
+    extends: ["js/recommended"],
     languageOptions: {
       globals: globals.browser,
     },
     rules: {
-      semi: ['error', 'always'],
-      quotes: ['error', 'single', { avoidEscape: true }],
+      semi: ["error", "always"],
+      quotes: ["error", "single", { avoidEscape: true }],
     },
   },
 ]);
-
-
