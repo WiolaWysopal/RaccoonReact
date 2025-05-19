82a4ed4aade3850b66e46e48b724748d5ce2d9a7
wwysopal <wioletta.wysopal@gmail.com>
2025-05-19 10:07:42 +0200

    Create DynamicAPIRenderer directory

---
+++ DynamicAPIRenderer/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ItemList from "./ItemList";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>List of Items:</h1>
+      <ItemList />
+    </div>
+  );
+};
+
+export default App;
---
+++ DynamicAPIRenderer/src/ItemList.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+const ItemList = () => {
+  const [items, setItems] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Błąd podczas pobierania danych");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setItems(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Data Loading...</p>;
+  }
+
+  if (error) {
+    return <p>An error occurred: {error}</p>;
+  }
+
+  if (items.length === 0) {
+    return <p>No data to display.</p>;
+  }
+
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default ItemList;
