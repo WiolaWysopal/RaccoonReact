5da72a26b67e24f614eb823d5e13665cf5566c89
wwysopal <wioletta.wysopal@gmail.com>
2025-05-20 09:55:47 +0200

    Create HelloComponent directory

---
+++ HelloComponent/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import LogHelloWorld from "./Hello";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Greetings</h1>
+      <LogHelloWorld />
+    </div>
+  );
+}
+
+export default App;
---
+++ HelloComponent/src/Hello.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function LogHelloWorld() {
+  const greetingVariable = "Hello world!";
+
+  return <h2>{greetingVariable}</h2>;
+}
+
+export default LogHelloWorld;
