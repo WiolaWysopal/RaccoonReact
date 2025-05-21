79ddc293219180b7523611d8f05a764b66d7d8c0
wwysopal <wioletta.wysopal@gmail.com>
2025-05-21 10:12:31 +0200

    StaticComponentRenderer

---
+++ StaticComponentRenderer/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import StaticText from "./StaticComponent";
+
+function App() {
+  return (
+    <div class="App">
+      <h1>Basic Component Rendering</h1>
+      <StaticText />
+    </div>
+  );
+}
+
+export default App;
---
+++ StaticComponentRenderer/src/StaticComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StaticText = () => {
+  return <p>"This is static text component!" </p>;
+};
+
+export default StaticText;
