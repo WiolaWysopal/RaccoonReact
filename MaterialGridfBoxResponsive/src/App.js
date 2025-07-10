1509b3670e62fbdb9fbe6a39d9b74ea6e35c1bac
wwysopal <wioletta.wysopal@gmail.com>
2025-07-10 10:18:28 +0200

    Create MaterialGridBoxResponsive directory

---
+++ MaterialGridfBoxResponsive/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SimpleLayout from "./SimpleLayout";
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleLayout />
+    </div>
+  );
+}
+
+export default App;
