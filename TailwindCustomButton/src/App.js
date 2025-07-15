41b9b71606ec7350f67c9b04800e1bd847dbe436
wwysopal <wioletta.wysopal@gmail.com>
2025-07-15 10:12:35 +0200

    Create TailwindCustomButton directory

---
+++ TailwindCustomButton/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import CustomButton from "./CustomButton";
+
+function App() {
+  return <CustomButton />;
+}
+
+export default App;
---
+++ TailwindCustomButton/src/CustomButton.js
@@ -0,0 +1,11 @@
+const CustomButton = () => {
+  return (
+    <div class="flex items-center justify-center h-screen">
+      <button className="bg-blue-500 hover:bg-blue-700 focus:outline-2 focus:outline-offset-2 focus:outline-white focus:ring-4 focus:ring-violet-500 active:bg-violet-700 text-white font-bold py-2 px-4 rounded m-4">
+        Loading
+      </button>
+    </div>
+  );
+};
+
+export default CustomButton;
